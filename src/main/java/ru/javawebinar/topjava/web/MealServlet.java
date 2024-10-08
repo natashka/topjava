94dd58a50cb8445ad0a985d29618e0e35a26c2c3
nataa <nataaviagis@gmail.com>
2024-10-08 18:34:05 +0300

    Fix after review

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -21,8 +21,8 @@ import static ru.javawebinar.topjava.util.MealsUtil.filteredByStreams;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
-    private static final int caloriesPerDay = 2005;
-    List<Meal> meals = Collections.synchronizedList((Arrays.asList(
+    private static final int CALORIES_PER_DAY = 2005;
+    private final List<Meal> meals = Arrays.asList(
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
@@ -30,15 +30,14 @@ public class MealServlet extends HttpServlet {
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
             new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
-    )));
+    );
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         List<MealTo> mealsTo = Collections.synchronizedList(filteredByStreams(meals,
                 LocalTime.of(0, 0),
                 LocalTime.of(23, 59),
-                caloriesPerDay));
+                CALORIES_PER_DAY));
         request.setAttribute("meals", mealsTo);
-        log.info(System.getenv("TOPJAVA_ROOT"));
         request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -40,7 +40,7 @@
     </thead>
     <tbody>
     <c:forEach items="${meals}" var="meal">
-        <tr style="color:${meal.excess ? 'green' : 'red'}">
+        <tr style="color:${meal.excess ? 'red' : 'green'}">
             <fmt:parseDate value="${meal.dateTime}" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDateTime" type="both"/>
             <td><fmt:formatDate pattern="yyyy-MM-dd HH:mm" value="${ parsedDateTime }"/></td>
             <td>${meal.description}</td>
